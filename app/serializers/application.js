ba477022c2f0751a8c8ae652015c7688ecc7b9e8
Lakshmi Padmavathi Nunna <lnunna@FWS-CHE-LT-6694.local>
2021-12-16 12:10:04 +0530

    listed data from json file

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,5 +1,3 @@
 import DS from 'ember-data';
-export default DS.JSONAPIAdapter.extend({
-    host: 'https://jsonplaceholder.typicode.com',
-    namespace: 'api/v1'
+export default DS.JSONSerializer.extend({
 });
