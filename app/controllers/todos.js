76fa206e298325bc49a79cd1b05cc72d45d47054
lakshmi nunna <lakshmi.nunna@freshworks.com>
2021-12-31 15:43:17 +0530

    filtered todos

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -2,6 +2,22 @@ import Ember from 'ember';
 // import todo from '../models/todo';
 
 export default Ember.Controller.extend({
+  queryParams: ['completed'],
+  completed: null,
+  filteredTodos: Ember.computed('completed', 'model', function() {
+    var completed = this.get('completed');
+    var todos = this.get('model');
+  
+    if (completed === "true") {
+      return todos.filterBy('completed', true);
+    } else if (completed === "false") {
+      return todos.filterBy('completed', false);
+    } else {
+      return todos;
+    }
+  }),
+
+
   actions: {
     addTodo() {
       if (this.get('description')) {
